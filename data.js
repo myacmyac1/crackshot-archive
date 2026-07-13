5a5cb934250591a9be9d620d231685ea65df6cc7
myacmyac1 <myacmyac1@gmail.com>
2026-07-13 22:57:44 +0900

    Update data.js

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ const CONCERTS = [
     photoUrl: "https://www.instagram.com/p/DanYcARgSuZ/?utm_source=ig_web_copy_link",
     setlist: [
       "Get Over",
-      "Loud Hot Crazy",
+      "LOUD! HOT! CRAZY!",
       "Puppeteer",
       "Bang Your Head",
       "Young & Wild",
@@ -34,9 +34,9 @@ const CONCERTS = [
       "DASH",
       "Puppeteer",
       "Take it down",
-      "Shake it",
+      "Shake It!",
       "INFP",
-      "Loud Hot Crazy",
+      "LOUD! HOT! CRAZY!",
       "Follow me",
       "Paradise city"
     ]
@@ -54,13 +54,101 @@ const CONCERTS = [
       "Warriors",
       "Fire",
       "Time to Rise",
-      "Shake it",
+      "Shake It!",
       "Overdrive",
       "Bye Goodbye",
       "Follow me",
-      "INFP with.더픽스",
-      "Don't Stop Believin",
-      "Don't Look Back"
+      "INFP w.더픽스",
+      "Don't Stop Believin w.더픽스",
+      "Don't Look Back w.더픽스"
+    ]
+  },
+  {
+    id: "2026-04-25",
+    date: "2026-04-25",
+    title: "2026 Hero Rock Festival",
+    venue: "마포 문화비축기지",
+    photoUrl: "https://www.instagram.com/p/DXi0UCVkWqk/?utm_source=ig_web_copy_link",
+    setlist: [
+      "Go Away",
+      "Young & Wild",
+      "Get Over",
+      "Overdrive",
+      "LOUD! HOT! CRAZY!",
+      "Shake It!",
+      "INFP",
+      "Follow me"
+    ]
+  },
+    {
+    id: "2026-04-17",
+    date: "2026-04-17",
+    title: "2026 ONE HOUR 4월",
+    venue: "부산 상상마당",
+    photoUrl: "https://www.instagram.com/p/DXO7xQggeRL/?utm_source=ig_web_copy_link",
+    setlist: [
+      "Go Away",
+      "Get Over",
+      "Young & Wild",
+      "Overdrive",
+      "Midnight Crime",
+      "Don't Close Your Eyes",
+      "Time To Rise",
+      "My Friend",
+      "Shake It!",
+      "Slow Motion",
+      "INFP",
+      "LOUD! HOT! CRAZY!",
+      "Bye Goodbye",
+      "Break Down",
+      "Follow Me"
+    ]
+  },
+  {
+    id: "2026-03-13",
+    date: "2026-03-13",
+    title: "크랙샷 단독 콘서트 백열",
+    venue: "홍대 웨스트브릿지",
+    photoUrl: "https://www.instagram.com/p/DV1BEBDAdJe/?utm_source=ig_web_copy_link",
+    setlist: [
+      "Go Away",
+      "DASH",
+      "Get Over",
+      "Young & Wild",
+      "Shake It!",
+      "Night Rider",
+      "Overdrive",
+      "Party! Party!",
+      "Suzie Q",
+      "Slow Motion",
+      "Slither",
+      "Warriors",
+      "The Show Must Go On",
+      "Time To Rise",
+      "Goodbye My Love",
+      "Take it Down",
+      "INFP",
+      "LOUD! HOT! CRAZY!",
+      "Bye Goodbye",
+      "Break Down",
+      "Follow Me"
+    ]
+  },
+  {
+    id: "2026-01-17",
+    date: "2026-01-17",
+    title: "너트 30 페스티벌",
+    venue: "홍대 상상마당",
+    photoUrl: "https://www.instagram.com/p/DTnYkL1gXAU/?utm_source=ig_web_copy_link",
+    setlist: [
+      "LOUD! HOT! CRAZY!",
+      "Young & Wild",
+      "Overdrive",
+      "Shake It!",
+      "INFP",
+      "Follow Me",
+      "Party! Party!",
+      "넌 내게 반했어"
     ]
   }
 ];
